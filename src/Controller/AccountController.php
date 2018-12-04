2cfbdb512b7a67f797678813daed7e05eca9fe24
Kza <Rukza@orange.fr>
2018-12-04 16:45:44 +0100

    ajout Fonction connexion/déco et controller

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,18 +2,78 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
     /**
+     * Permet l'affichage et le formulaire de connexion'
+     * 
      * @Route("/login", name="account_login")
+     * 
+     * @return Response
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
         return $this->render('account/login.html.twig', [
-          
+                'hasError' => $error !== null,
+                'username' => $username
+        ]);
+    }
+    /**
+     * Permet gérer la déconnection 
+     * @Route("/logout", name="account_logout")
+     *
+     * @return void
+     */
+    public function logout(){
+            //
+    }
+
+  /**
+     * Permet d'afficher le formulaire d'insciption
+     * 
+     * @Route("/register", name="account_register")
+     *
+     * @return Response
+     */
+    public function register(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder){
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request); 
+
+        if($form->isSubmitted() && $form->isValid()){
+
+        $hash = $encoder->encodePassword($user, $user->getPswd());
+        $user->setPswd($hash);
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "votre compte a bien été crée!"
+
+            
+        );
+        return $this->redirectToRoute('account_login');
+        }
+
+        return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegistrationType extends AbstractType
+{
+    private function getConfiguration($label, $placeholder){
+        return [
+            'label' => $label,
+            'attr'  =>[
+                'placeholder' => $placeholder
+            ]
+            ];
+    }
+        /**
+         * Configuration de base d'un champ
+         *
+         * @param string $label
+         * @param string $placeholder
+         * @param array $options
+         * 
+         * @return array
+         */
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName',TextType::class,$this->getConfiguration("Prénom", "Veuillez renseigner votre prénom"))
+            ->add('lastName',TextType::class,$this->getConfiguration("Nom", "Veuillez renseigner votre nom"))
+            ->add('email',EmailType::class,$this->getConfiguration("Email", "Veuillez renseigner votre email"))
+            ->add('pswd',PasswordType::class,$this->getConfiguration("Mot de passe", "Veuillez renseigner votre mot de passe"))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
