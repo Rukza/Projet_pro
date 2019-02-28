be8c4365a83d6dc035bb52987e39d49ccafb16b6
julien casanova <Rukza@orange.fr>
2019-02-28 14:43:58 +0100

    Ajout protection form login vs attaque csrf

--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -22,6 +22,12 @@ class AccountType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'csrf_protection' => true,
+            // the name of the hidden HTML field that stores the token
+            'csrf_field_name' => '_token',
+            // an arbitrary string used to generate the value of the token
+            // using a different string for each form improves its security
+            'csrf_token_id'   => 'User_item',
         ]);
     }
 }
